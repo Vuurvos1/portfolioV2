8e5d5b6eb839af44487b64b7c8f2187ff88faa37
Firefox3000 <Firefox3000@users.noreply.github.com>
2019-11-12 22:47:58 +0100

    file structure and content

--- js/script.js
+++ js/script.js
@@ -2,15 +2,3 @@
 document.querySelector('#burgerMenu').addEventListener('click', function() {
     document.querySelector('nav').classList.toggle('menuActive');
 });
-
-/*
-// Back to top button
-document.querySelector('.topButton').addEventListener('scroll', function() {
-    console.log('yeet scrool');
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.querySelector('.topButton').style.display = 'block';
-    } else {
-        document.querySelector('.topButton').style.display = 'none';
-    }
-});
-*/
