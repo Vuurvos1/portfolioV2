f2b02565a2dec1c29fba1dfdd015e1e8d4427933
Sam de Man <47493725+Firefox3000@users.noreply.github.com>
2019-11-13 17:09:55 +0100

    Nav bar update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 // Burger menu toggle
-document.querySelector('#burgerMenu').addEventListener('click', function() {
+document.querySelector('#burgerMenu').addEventListener('click', function () {
     document.querySelector('nav').classList.toggle('menuActive');
 });
+
+//hide nav on scroll
+let prevScroll = window.pageYOffset;
+window.onscroll = function() {
+    let currentScroll = window.pageYOffset;
+    if (currentScroll > 75 && !document.querySelector('nav').classList.contains('menuActive')) {
+        if (prevScroll > currentScroll) {
+            document.querySelector('header').style.top = '0';
+        } else {
+            document.querySelector('header').style.top = '-4rem';
+        }
+    }
+    prevScroll = currentScroll;
+}
+
+
