63715e1d25141828d02d396c22b4e341771e952e
Sam de Kanter <samdepanter@gmail.com>
2020-09-04 13:17:57 +0200

    added tap outside menu to close menu

--- static/js/navSwipe.js
+++ static/js/navSwipe.js
@@ -13,16 +13,20 @@ let menuOpen = false;
 let menuWidth = menuList.offsetWidth;
 
 const menuOpenDrag = 80;
+const mobileWidht = 768;
 
-menuList.style.transform = `translateX(${menuWidth + 32}px)`;
-
-// Get and update window width
-
+// Get window width
 let windowWidth =
   window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
 
+// Setup code
+if (windowWidth < mobileWidht) {
+  menuList.style.transform = `translateX(${menuWidth + 32}px)`;
+}
+
+// window resize
 window.onresize = () => {
   windowWidth =
     window.innerWidth ||
@@ -32,6 +36,12 @@ window.onresize = () => {
   console.log(`windowWidth: ${windowWidth}`);
 
   menuWidth = menuList.offsetWidth;
+
+  if (windowWidth > mobileWidht) {
+    menuList.style.transform = `translateX(0px)`;
+  } else {
+    menuList.style.transform = `translateX(${menuWidth + 32}px)`;
+  }
 };
 
 // Menu toggle
@@ -67,9 +77,7 @@ trigger.addEventListener(
     dragDist = touchStartX - touchmoveX;
 
     if (dragDist > 0 && dragDist < menuOpenDrag) {
-      menuOpen = false;
-      menuCheckbox.checked = false;
-      menuList.style.transform = `translateX(${menuWidth - dragDist + 32}px)`;
+      menuOpen(false);
     }
   },
   false
@@ -81,13 +89,34 @@ trigger.addEventListener("touchend", (e) => {
   console.log(e);
   if (dragDist < menuOpenDrag) {
     // close menu
-    menuOpen = false;
-    menuCheckbox.checked = false;
-    menuList.style.transform = `translateX(${menuWidth + 32}px)`;
+    openMenu(false);
   } else {
     // open menu
+    openMenu(true);
+  }
+});
+
+// tap next to menu when open
+document.querySelector("body").addEventListener("click", (e) => {
+  if (menuOpen) {
+    if (e.clientX < windowWidth - menuWidth) {
+      // close menu
+      openMenu(false);
+    }
+  }
+});
+
+// add swipe to close menu
+
+function openMenu(a) {
+  // true is open menu, false is close menu
+  if (a) {
     menuOpen = true;
     menuCheckbox.checked = true;
     menuList.style.transform = `translateX(0)`;
+  } else {
+    menuOpen = false;
+    menuCheckbox.checked = false;
+    menuList.style.transform = `translateX(${menuWidth + 32}px)`;
   }
-});
+}
