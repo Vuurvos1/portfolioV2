b7e28615f2aab6069a7683c84fd663eb16c22927
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2021-02-24 16:49:41 +0000

    deploy: d73f6537b97156c60d59a54a06c60478813a2f76

--- _nuxt/static/1614180257/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002F","\u002Fprojects","\u002Fprojects\u002FhikingApp","\u002Fprojects\u002FiCov","\u002Fprojects\u002FformFun","\u002Fprojects\u002FmealMatch","\u002Fprojects\u002FpartyPickle","\u002Fprojects\u002FsewersGame","\u002Fprojects\u002Ftictactoe","\u002Fprojects\u002FstayHydratedBot","\u002Fprojects\u002Fvase"]})
---
+++ _nuxt/static/1614185380/projects/stayHydratedBot/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614185380",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002FstayHydratedBot",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a58eb03f"}}}}(null));
