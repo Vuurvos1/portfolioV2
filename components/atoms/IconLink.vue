52170bfb90b194fe55dfba2218ca2795bf238e30
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-04-01 20:48:16 +0200

    playing with components

---
+++ components/atoms/IconLink.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: "/",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<template>
+  <a :href="href">
+    <img :src="`./icons/${icon}.svg?inline`" />
+    <slot>Link</slot>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+a {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0.25rem 1rem 0.25rem 0.75rem;
+  font-size: 0.875rem;
+  background: #6200ee;
+  border-radius: 0.25rem;
+  color: #fff;
+
+  img {
+    margin-right: 0.5rem;
+    height: 1.125rem;
+    width: auto;
+    filter: invert(1);
+  }
+}
+</style>
---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <IconLink :href="'pornhub.com'" icon="launch">sapjes</IconLink>
+  </main>
+</template>
+
+<script>
+import IconLink from "../../components/atoms/IconLink.vue";
+
+export default {
+  components: { IconLink },
+};
+</script>
