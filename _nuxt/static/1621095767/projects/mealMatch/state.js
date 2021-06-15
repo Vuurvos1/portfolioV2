82a830b738d06a5409b4f8043964f6d74f053a1e
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-06-15 21:09:24 +0000

    Deploying to gh-pages from @ Vuurvos1/portfolio@d7f897ee0975c733490c65c402d8c38c67c10fbb 🚀

--- _nuxt/static/1621095767/projects/mealMatch/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621095767",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002FmealMatch",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"499c4514"}}}}(null));
---
+++ _nuxt/static/1623791363/projects/iCov/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623791363",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002FiCov",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9d64e6ca"}}}}(null));
